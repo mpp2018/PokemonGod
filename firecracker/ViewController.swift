4c7abc7a4266290be79d9f41bf5892d1cfdca66c
Huaiyu <sirius733@gmail.com>
2018-06-01 11:11:17 +0800

    setup ARSCNView, delegate, configuration, plan detection

--- firecracker/ViewController.swift
+++ firecracker/ViewController.swift
@@ -9,38 +9,97 @@ import ARKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var sceneView:ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupScene()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sceneStart()
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    func setupScene() {
+        sceneView = ARSCNView(frame: self.view.bounds)
+        sceneView.showsStatistics = true
+        sceneView.scene = SCNScene()
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        sceneView.delegate = self
+        sceneView.scene.physicsWorld.contactDelegate = self
+        configureLighting()
+        self.view.addSubview(sceneView)
+    }
+ 
+    func sceneStart() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.vertical, .horizontal]
+        sceneView.session.run(configuration)
+    }
+    func configureLighting() {
         
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
     }
     
+}
+
+
+
+extension ViewController: ARSCNViewDelegate {
+ 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let  planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.y)
+        let plane = SCNPlane(width: width, height: height)
+        
+        plane.materials.first?.diffuse.contents = UIColor.init(red: 0.6, green: 0.6, blue: 1, alpha: 0.5)
+        let planeNode = SCNNode(geometry: plane)
+        
+        let position = SCNVector3(planeAnchor.center.x,
+                                planeAnchor.center.y,
+                                planeAnchor.center.z)
+        
+        planeNode.position = position
+        planeNode.eulerAngles.x = -.pi/2
+        node.addChildNode(planeNode)
         
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         
     }
-    func setupScene() {
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+    }
+}
+
+extension ViewController:SCNPhysicsContactDelegate {
+    
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
         
     }
     
-}
-
-
-
-extension ViewController: ARSCNViewDelegate, SCNPhysicsContactDelegate, SCNSceneRendererDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didUpdate contact: SCNPhysicsContact) {
+        
+    }
     
+    func physicsWorld(_ world: SCNPhysicsWorld, didEnd contact: SCNPhysicsContact) {
+        
+    }
 }
-
 
