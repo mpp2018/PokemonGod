538e3c1cf61751ad2bc213c07c66aaa967d3a5e6
Huaiyu <sirius733@gmail.com>
2018-06-01 10:40:04 +0800

    setup for ARkit

--- firecracker/ViewController.swift
+++ firecracker/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Huaiyu.Lin on 2018/6/1.
 //  Copyright © 2018 Huaiyu Lin. All rights reserved.
 //
-
+import ARKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -20,6 +20,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+    }
+    func setupScene() {
+        
+    }
+    
 }
+
+
+
+extension ViewController: ARSCNViewDelegate, SCNPhysicsContactDelegate, SCNSceneRendererDelegate {
+    
+}
+
 
