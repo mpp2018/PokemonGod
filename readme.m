8d689c3ae2696495e708da5b3e195849413e92d5
Huaiyu <huaiyulin1026@gmail.com>
2018-06-18 01:56:41 +0800

    add readme file

---
+++ readme.m
@@ -0,0 +1,27 @@
+### Motivation
+Traditional customs may cause environmental pollution and are disappearing gradually. We want to reserve these traditions in a virtual way (with AR) , which will not destroy the environment. 
+
+
+### Introduction
+- Firecracker
+	1. Detect a horizontal plane
+	2. Touch the plane, and virtual firecrackers will appear.
+	3. Drag on the screen to determine the length of firecrackers
+	4. Click “Explode” button to Light firecrackers
+	5. firecrackers explode with sound and virtual explosion fire.
+
+- Firework
+	1. Detect a horizontal plane
+	2. Touch the plane, and virtual firework will appear.
+	3. Click “Fire” button to light up the fireworks
+	4. Click “Shoot” button, then fireworks will shoot to the sky.
+
+- MoonBlock
+	1. Detect a horizontal plane
+	2. Swipe on the screen, and the virtual moon blocks will be cast to the plane
+	3. After physical contact, show the result（笑筊, 聖筊, 陰筊, 立筊）
+
+- PaperMoney
+	1. Detect a horizontal plane and virtual bucket will be append to the plane.
+	2. Throw virtual paper money into the bucket to keep the fire
+	3. After not throwing paper into bucket for a while, fire will slowly extinguish
